a4de8fbf60bf33b0f5bb66b679c22f7f30e613be
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-03 22:34:32 +0300

    add listener and render function

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,46 @@ const refs = {
   form: document.querySelector('.search-form'),
   galleryContainer: document.querySelector('.gallery'),
 };
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit() {}
+
+function renderGallery(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        `<a class="gallery__link" href="${largeImageURL}">
+        <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                <b>Likes</b>
+                ${likes}
+                </p>
+                <p class="info-item">
+                <b>Views</b>
+                ${views}
+                </p>
+                <p class="info-item">
+                <b>Comments</b>
+                ${comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads</b>
+                ${downloads}
+                </p>
+            </div>
+        </div>`;
+      }
+    )
+    .join('');
+}
