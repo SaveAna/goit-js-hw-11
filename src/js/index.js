ec275d8bfe9a06f8c995d7a9ed3653b5db4a85b7
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-03 22:37:32 +0300

    fix markup function

--- src/js/index.js
+++ src/js/index.js
@@ -29,27 +29,28 @@ function renderGallery(images) {
         downloads,
       }) => {
         `<a class="gallery__link" href="${largeImageURL}">
-        <div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                <b>Likes</b>
-                ${likes}
-                </p>
-                <p class="info-item">
-                <b>Views</b>
-                ${views}
-                </p>
-                <p class="info-item">
-                <b>Comments</b>
-                ${comments}
-                </p>
-                <p class="info-item">
-                <b>Downloads</b>
-                ${downloads}
-                </p>
+            <div class="photo-card">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                    <p class="info-item">
+                        <b>Likes</b>
+                        ${likes}
+                    </p>
+                    <p class="info-item">
+                        <b>Views</b>
+                        ${views}
+                    </p>
+                    <p class="info-item">
+                        <b>Comments</b>
+                        ${comments}
+                    </p>
+                    <p class="info-item">
+                        <b>Downloads</b>
+                        ${downloads}
+                    </p>
+                </div>
             </div>
-        </div>`;
+        </a>`;
       }
     )
     .join('');
