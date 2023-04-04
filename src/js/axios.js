8b15aa6381b5cb947e87845d389419f2df9be5e3
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-04 10:00:07 +0300

    add onInput func (problem with markup render)

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
-// axios.defaults.headers.common['Authorization'] =
-//   '35041126-c969f82855a4c8ea112aa3b64';
 const API_KEY = '35041126-c969f82855a4c8ea112aa3b64';
 
 async function axiosAPI(name, page, perPage) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,20 @@
 import '../css/styles.css';
+import debounce from 'lodash.debounce';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { axiosAPI } from './axios';
 
 const refs = {
   form: document.querySelector('#search-form'),
   galleryContainer: document.querySelector('.gallery'),
   loadBtn: document.querySelector('.load-more'),
 };
+
+let simpleLightBox = new SimpleLightbox('.gallery .gallery__link');
+let name = '';
+let page = 1;
+const perPage = 40;
 
 refs.loadBtn.style.display = 'none';
 
@@ -16,15 +23,35 @@ refs.loadBtn.addEventListener('click', onClick);
 
 function onSubmit(event) {
   event.preventDefault();
-  refs.gallery.innerHTML = '';
-  const name = event.target.value.trim();
-  if (name !== '') {
-    return queryImage(name);
+
+  page = 1;
+  name = event.currentTarget.searchQuery.value.trim();
+  console.log(name);
+  if (!name) {
+    return;
   }
-  refs.loadBtn.style.display = 'none';
-  return Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
+  refs.galleryContainer.innerHTML = '';
+  axiosAPI(name, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        refs.loadBtn.style.display = 'none';
+        refs.galleryContainer.innerHTML = '';
+        return Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else if (data.totalHits > perPage) {
+        refs.loadBtn.style.display = 'block';
+      } else if (data.totalHits < perPage && data.totalHits != 0) {
+        refs.loadBtn.style.display = 'none';
+      }
+      createMarkup(data.hits);
+      console.log(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery .gallery__link').refresh();
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    })
+    .catch(error => {
+      return 0;
+    });
 }
 
 function onClick() {}
@@ -42,29 +69,36 @@ function renderGallery(images) {
         downloads,
       }) => {
         `<a class="gallery__link" href="${largeImageURL}">
-            <div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                <div class="info">
-                    <p class="info-item">
-                        <b>Likes</b>
-                        ${likes}
-                    </p>
-                    <p class="info-item">
-                        <b>Views</b>
-                        ${views}
-                    </p>
-                    <p class="info-item">
-                        <b>Comments</b>
-                        ${comments}
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads</b>
-                        ${downloads}
-                    </p>
-                </div>
-            </div>
-        </a>`;
+             <div class="photo-card">
+                 <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                 <div class="info">
+                     <p class="info-item">
+                         <b>Likes</b>
+                         ${likes}
+                     </p>
+                     <p class="info-item">
+                         <b>Views</b>
+                         ${views}
+                     </p>
+                     <p class="info-item">
+                         <b>Comments</b>
+                         ${comments}
+                     </p>
+                     <p class="info-item">
+                         <b>Downloads</b>
+                         ${downloads}
+                     </p>
+                 </div>
+             </div>
+         </a>`;
       }
     )
     .join('');
+}
+
+function createMarkup(markup) {
+  return refs.galleryContainer.insertAdjacentHTML(
+    'beforeend',
+    renderGallery(markup)
+  );
 }
