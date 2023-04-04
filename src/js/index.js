143db3be4c1ac94834329aa54ae457061d41c814
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-04 10:13:38 +0300

    add smoothScroll func

--- src/js/index.js
+++ src/js/index.js
@@ -116,3 +116,14 @@ function createMarkup(markup) {
     renderGallery(markup)
   );
 }
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
